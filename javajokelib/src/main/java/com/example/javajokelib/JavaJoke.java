7a569ea5318efd82093f90930088544eca24b503
FlixKo <f.kohlbrenner90@gmail.com>
2020-07-15 17:02:38 +0200

    incorporate changes

--- javajokelib/src/main/java/com/example/javajokelib/JavaJoke.java
+++ javajokelib/src/main/java/com/example/javajokelib/JavaJoke.java
@@ -1,8 +1,19 @@
 package com.example.javajokelib;
 
+import java.util.Random;
+
 public class JavaJoke {
 
+    static String [] jokesArray = {
+            "How do functions break up? \"They stop calling each other!\"",
+            "In what unit do you measure a function's length? \"In Para meters...\"",
+            "What did the flirtatious function say? \"Call me ;)\"",
+            "How do you tell an introverted computer scientist from an extroverted computer scientist?\n" +
+                    "\n" +
+                    "A: An extroverted computer scientist looks at your shoes when he talks to you."
+    };
+
     public static String getJoke(){
-        return "This is a joke!";
+        return jokesArray[new Random().nextInt(jokesArray.length)];
     }
 }
